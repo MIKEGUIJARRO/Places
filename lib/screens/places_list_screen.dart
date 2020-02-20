51198e5d056bd7aa4621fc0d8ce9941d04c063c8
Mike Guijarro <a01612042@itesm.mx>
2020-02-19 23:09:53 -0600

    Primer commit e instanciacion de ImagePicker

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import './add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Places"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          )
+        ],
+      ),
+
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  ImageInput({Key key}) : super(key: key);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageField =
+        await ImagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            onPressed: _takePicture,
+            icon: Icon(Icons.camera),
+            label: Text(
+              "Take Picture",
+              textAlign: TextAlign.center,
+            ),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        )
+      ],
+    );
+  }
+}
